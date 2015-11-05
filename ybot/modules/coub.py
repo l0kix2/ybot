d2cb7702a14a1563aa01172a34c9ea5df1ae123f
Kirill SIbirev <l0kix2@gmail.com>
2015-11-05 12:49:31 +0300

    Change url for coub weekly

--- ybot/modules/coub.py
+++ ybot/modules/coub.py
@@ -1,6 +1,4 @@
 # coding: utf-8
-from datetime import datetime, timedelta
-
 from ybot.conf import settings
 from .telegram import bot
 from .cron import cron
@@ -10,9 +8,7 @@ conf = settings[__name__]
 
 @cron('ybot.coub_weekly', '0 16 * * fri')
 def send_weekly(name, value):
-    last_fiday = datetime.now() - timedelta(7)
-    url = 'https://coub.com/weekly/{year}/{week}'
-    year, week, _ = last_fiday.isocalendar()
+    url = 'https://coub.com/weekly/'
 
     ids = conf.get('chat_ids', ())
     for chat_id in ids:
